0cf0a7c5ec0069bc5da7ff334f34acbd77e380fe
YamaKentucky <yamaken0114@icloud.com>
2022-06-16 21:28:50 +0900

    a

--- signal.js
+++ signal.js
@@ -1,6 +1,6 @@
 let colorFlg=0;
 var now = new Date();
-var tim_red =124;
+var tim_red =10;//124;
 var tim_blue = 20;
 var base_time=40500;//11:15:00
 
@@ -25,7 +25,9 @@ function changeColor(idname,flg){
 }
 
 
-timerID = setInterval('clock()',500); //0.5秒毎にclock()を実行
+// timerID = 
+setInterval('clock()',300); //0.5秒毎にclock()を実行
+setInterval('time_count()',1000); //0.5秒毎にclock()を実行
 
 
 function clock() {
@@ -45,13 +47,24 @@ function getNow() {
     //信号タイミング計算
     var t_sec = (3600*hour + 60*min + sec)-base_time;
     if ((t_sec-tim_red)%(tim_blue+tim_red)<=tim_blue){
+        document.getElementById("timer-time").textContent =tim_blue- (t_sec-tim_red)%(tim_blue+tim_red);
         changeColor("left_signal_svg",1);
     }else if(t_sec%(tim_blue+tim_red)<=tim_red){
+        document.getElementById("timer-time").textContent =tim_red - (t_sec)%(tim_blue+tim_red);
         changeColor("left_signal_svg",0);
     }
-
+    
 	var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒" ; 
     
 	return s;
 }
+
+
+// var i = 1
+// var time=10
+// function time_count() {
+//     var count = time-i;
+// 	document.getElementById("timer-time").textContent = count;
+//     i++;
+// }
 
